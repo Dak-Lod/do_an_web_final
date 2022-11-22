3b3d32c71fea5c31ed548cdec4b3496faa9a1c12
Dak Lod <trandacloc123@gmail.com>
2022-11-22 12:23:41 +0700

    loc update

--- js/admin.js
+++ js/admin.js
@@ -35,6 +35,77 @@ let popup_edt;
 const currentView = location.href.split('?')
 let popup_rmv_account;
 let popup_edt_account;
+let popup_rmv_cate;
+let popup_edt_cate;
+
+function cate_ren(categories){
+    var table = document.querySelector('#cate-table > tbody')
+    table.parentElement.childNodes[4].remove()
+    table = document.createElement('tbody')
+    document.querySelector('#cate-table').append(table)
+    categories.forEach((ele, index) => {
+        if (ele == null) return 
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+        td.textContent = index
+        tr.append(td)
+
+        td = document.createElement('td')
+        td.textContent = ele
+        tr.appendChild(td)
+        
+        td = document.createElement('td')
+        
+        let btn = document.createElement('button')
+        let i = document.createElement('i')
+        i.classList.add('fa-sharp', 'fa-solid', 'fa-gear')
+        btn.appendChild(i)
+        btn.setAttribute('class', 'btn-edt')
+        td.appendChild(btn) 
+
+        btn.addEventListener('click', function(e){
+            const blur = document.getElementById('blur')
+            blur.classList.toggle('active')
+            popup_edt_cate.classList.toggle('active')
+            popup_edt_cate.style.top = "50%"
+
+            const info = e.currentTarget.parentNode.parentNode.children
+            const index = info[0].textContent
+            const popup_info = popup_edt_cate.children[2].children[0].children
+            popup_info[0].children[1].value = index
+            popup_info[1].children[1].value = categories[index].name
+
+
+        })
+        
+        
+        btn = document.createElement('button')
+        btn.innerHTML = `<i class="fa-sharp fa-solid fa-circle-xmark"></i>`
+        btn.setAttribute('class', 'btn-rmv')
+        btn.addEventListener('click', function(e){
+            const blur = document.getElementById('blur')
+            blur.classList.toggle('active')
+            popup_rmv_account.classList.toggle('active')
+            popup_rmv_account.style.top = "50%"
+            const info = e.currentTarget.parentNode.parentNode.children
+            const popup_info = popup_rmv_account.children[2].children
+
+            const id_rmv = info[0].textContent
+            const index = accounts.findIndex(({id})=>{
+                return id_rmv == id
+            })
+            popup_info[0].textContent = id_rmv
+            popup_info[1].textContent = accounts[index].name
+            
+
+
+        })
+        td.appendChild(btn)
+        tr.append(td)
+        table.append(tr)
+
+    })
+}
 
 function account_ren(accounts){
     var table = document.querySelector('#account-table > tbody')
@@ -75,6 +146,9 @@ function account_ren(accounts){
         btn.appendChild(i)
         btn.setAttribute('class', 'btn-edt')
         td.appendChild(btn) 
+        
+        
+
         btn.addEventListener('click', function(e){
             const blur = document.getElementById('blur')
             blur.classList.toggle('active')
@@ -95,10 +169,9 @@ function account_ren(accounts){
             popup_info[3].children[1].children[0].value = accounts[index].password
             popup_info[4].children[1].value = accounts[index].role
             
+            
 
         })
-
-        
         
         btn = document.createElement('button')
         btn.innerHTML = `<i class="fa-sharp fa-solid fa-circle-xmark"></i>`
@@ -159,14 +232,8 @@ function product_ren(products){
         tr.append(td)
         
         td = document.createElement('td')
-        // td.innerHTML = `<button class="btn-edt">
-        // <i class="fa-sharp fa-solid fa-gear"></i>
-        // </button>
-        // <br>
-        // <button class="btn-rmv">
-        // <i class="fa-sharp fa-solid fa-circle-xmark"></i>
-        // </button>`
-        
+    
+
         let btn = document.createElement('button')
         let i = document.createElement('i')
         i.classList.add('fa-sharp', 'fa-solid', 'fa-gear')
@@ -201,7 +268,7 @@ function product_ren(products){
         })
 
         
-        
+
         btn = document.createElement('button')
         btn.innerHTML = `<i class="fa-sharp fa-solid fa-circle-xmark"></i>`
         btn.setAttribute('class', 'btn-rmv')
@@ -240,7 +307,9 @@ function adminRen(){
     
     popup_edt_account = document.getElementById('popup-edt-account')
     popup_rmv_account = document.getElementById('popup-rmv-account')
-
+    
+    popup_edt_cate = document.getElementById('popup-edt-cate')
+    popup_rmv_cate = document.getElementById('popup-rmv-cate')
 
     document.querySelectorAll('.popup .cancel').forEach(ele=>{
         ele.addEventListener('click', disablePopup)
@@ -260,85 +329,6 @@ function adminRen(){
         // console.log(currentView[1].split('=')[1]);
     }
     switch (indexActive){
-        case '1':
-            document.querySelector('.manager.product').style.display = "flex"
-            product_ren(products)
-             //Add product
-
-            const id = Product.count + 1 + 10000
-            document.getElementById('id-product').value = id
-
-
-            const select = document.getElementsByClassName('cate-product')
-            const dataList = document.createElement('datalist')
-            
-            dataList.id = "cate-datalist"
-            
-            categories.forEach(function (ele){
-                const option = document.createElement('option')
-                option.value = ele
-                dataList.appendChild(option)
-            })
-            Array.prototype.forEach.call(select, ele => {
-                ele.append(dataList)
-                ele.setAttribute('list', 'cate-datalist')
-
-            })
-            
-
-            //Input check
-            document.getElementById('name-product').addEventListener('input', function (e){
-                if (e.target.value == "" || e.target.value.trim() == ""){
-                    e.target.setCustomValidity("Tên sản phẩm không được trống!")
-                    e.target.reportValidity()   
-                }else
-                    e.target.setCustomValidity("")
-            })
-
-            document.getElementById('cate-product').addEventListener('input', function (e){
-                if (e.target.value == "" || e.target.value.trim() == ""){
-                    e.target.setCustomValidity("Thể loại sản phẩm không được trống!")
-                    e.target.reportValidity()   
-                }else
-                    e.target.setCustomValidity("")
-            })
-
-
-
-            //add Image
-            const imgUpload = document.getElementsByClassName("img-product")
-            Array.prototype.forEach.call(imgUpload, ele=>{
-                ele.addEventListener("change", function (event) {
-                    const file = event.target.files[0]
-                    const img = event.currentTarget.parentElement.children[1]
-                    img.file = file
-                    img.style.display = "block";
-                    const reader = new FileReader();
-                    reader.onload = function (e){
-                        img.src = e.target.result
-                    }
-                    reader.readAsDataURL(file)
-            
-                })
-
-            })
-            // Popup btn remove
-            popup_rmv.children[3].children[0].onclick = function(){
-                const id_rmv =  popup_rmv.children[2].children[0].textContent
-                const index = products.findIndex(({id})=>{
-                    return id == id_rmv
-                })
-                products.splice(index, 1)
-                localStorage.setItem('products', JSON.stringify(products))
-                location.reload()
-            }
-
-            // Popup btn edit
-            popup_edt.children[3].children[0].onclick = function(){
-                const form = document.querySelector('#popup-edt form')
-                form.dispatchEvent(new Event('submit'))
-            }
-            break
         case '2':
             document.querySelector('.manager.account').style.display = "flex"
             account_ren(accounts)
@@ -396,6 +386,54 @@ function adminRen(){
                 location.reload()
 
             })
+            
+            // Popup btn remove
+            popup_rmv_account.children[3].children[0].onclick = function(){
+                const id_rmv =  popup_rmv.children[2].children[0].textContent
+                const index = accounts.findIndex(({id})=>{
+                    return id == id_rmv
+                })
+                accounts.splice(index, 1)
+                localStorage.setItem('acc', JSON.stringify(accounts))
+                location.reload()
+            }
+
+            // Popup btn edit
+            popup_edt_account.children[3].children[0].onclick = function(){
+                const form = document.querySelector('#popup-edt-account form')
+                form.dispatchEvent(new Event('submit'))
+            }
+            break
+            
+            case '3':
+                document.querySelector('.manager.cate').style.display = "flex"
+                cate_ren(categories)
+            // Account form add
+            let formCate = document.getElementById('form-cate')
+            console.log();
+            formCate.children[1].children[1].value = categories.length + 1
+            // Input check
+            formCate.children[2].children[1].addEventListener('input', function (e){
+                if (e.target.value == "" || e.target.value.trim() == ""){
+                    e.target.setCustomValidity("Tên thể loại không được trống!")
+                    e.target.reportValidity()   
+                }else
+                    e.target.setCustomValidity("")
+            })
+
+            
+            formCate.children[3].children[0].addEventListener('click', e=>{
+                
+                formCate.children[2].children[1].dispatchEvent(new Event('input'))
+                
+                const name = formCate.children[2].children[1].value
+
+                categories.push(name)
+                
+                localStorage.setItem('cate', JSON.stringify(categories))
+                location.reload()
+
+            })
 
             // Popup btn remove
             popup_rmv_account.children[3].children[0].onclick = function(){
@@ -413,11 +451,89 @@ function adminRen(){
                 const form = document.querySelector('#popup-edt-account form')
                 form.dispatchEvent(new Event('submit'))
             }
-
-            
-            
+        
+        
             break
             
+            default:
+                document.querySelector('.manager.product').style.display = "flex"
+                product_ren(products)
+                 //Add product
+        
+                const id = Product.count + 1 + 10000
+                document.getElementById('id-product').value = id
+        
+        
+                const select = document.getElementsByClassName('cate-product')
+                const dataList = document.createElement('datalist')
+                
+                dataList.id = "cate-datalist"
+                
+                categories.forEach(function (ele){
+                    const option = document.createElement('option')
+                    option.value = ele
+                    dataList.appendChild(option)
+                })
+                Array.prototype.forEach.call(select, ele => {
+                    ele.append(dataList)
+                    ele.setAttribute('list', 'cate-datalist')
+        
+                })
+                
+        
+                //Input check
+                document.getElementById('name-product').addEventListener('input', function (e){
+                    if (e.target.value == "" || e.target.value.trim() == ""){
+                        e.target.setCustomValidity("Tên sản phẩm không được trống!")
+                        e.target.reportValidity()   
+                    }else
+                        e.target.setCustomValidity("")
+                })
+        
+                document.getElementById('cate-product').addEventListener('input', function (e){
+                    if (e.target.value == "" || e.target.value.trim() == ""){
+                        e.target.setCustomValidity("Thể loại sản phẩm không được trống!")
+                        e.target.reportValidity()   
+                    }else
+                        e.target.setCustomValidity("")
+                })
+        
+        
+        
+                //add Image
+                const imgUpload = document.getElementsByClassName("img-product")
+                Array.prototype.forEach.call(imgUpload, ele=>{
+                    ele.addEventListener("change", function (event) {
+                        const file = event.target.files[0]
+                        const img = event.currentTarget.parentElement.children[1]
+                        img.file = file
+                        img.style.display = "block";
+                        const reader = new FileReader();
+                        reader.onload = function (e){
+                            img.src = e.target.result
+                        }
+                        reader.readAsDataURL(file)
+                
+                    })
+        
+                })
+                // Popup btn remove
+                popup_rmv.children[3].children[0].onclick = function(){
+                    const id_rmv =  popup_rmv.children[2].children[0].textContent
+                    const index = products.findIndex(({id})=>{
+                        return id == id_rmv
+                    })
+                    products.splice(index, 1)
+                    localStorage.setItem('products', JSON.stringify(products))
+                    location.reload()
+                }
+        
+                // Popup btn edit
+                popup_edt.children[3].children[0].onclick = function(){
+                    const form = document.querySelector('#popup-edt form')
+                    form.dispatchEvent(new Event('submit'))
+                }
+                break
     }
 
     cate_admin.forEach((ele, index) => {
@@ -425,18 +541,18 @@ function adminRen(){
         span.setAttribute('class', 'active');
         var link = document.createElement('a');
         if (index == 0)
-            link.setAttribute('href', './index.html')
+        link.setAttribute('href', './index.html')
         else {
             link.setAttribute('href', location.pathname + `?page=${index}`)
         }
         if (index == indexActive)
-            link.classList.toggle('active')
+        link.classList.toggle('active')
         link.innerText = ele
         navBar_cnt.appendChild(link)
     })
     
-
-
+    
+    
 
 
 
@@ -562,8 +678,7 @@ function addProduct(){
         name = name.value
         price = price.value
         cate = cate.value
-    
-    
+        
         if (categories.indexOf(cate) == -1){
             categories.push(cate)
             localStorage.setItem('cate', JSON.stringify(categories))
